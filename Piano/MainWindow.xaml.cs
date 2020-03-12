750ce8f36a7127b4d533b584391659460548a80f
alumno <alumno@E103>
2020-03-12 12:48:42 -0700

    Se empezo a usar el mixer

--- Piano/MainWindow.xaml.cs
+++ Piano/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
 namespace Piano
 {
     /// <summary>
@@ -20,9 +23,27 @@ namespace Piano
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WaveOut waveOut;
+        private MixingSampleProvider mixer;
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();waveOut = new WaveOut();
+            mixer = new MixingSampleProvider(WaveFormat.CreateIeeeFloatWaveFormat(44100, 1));
+            mixer.ReadFully = true;
+            waveOut.Init(mixer);
+            waveOut.Play();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var nota_do = new SignalGenerator(44100,1)
+            {
+                Gain = 0.8,
+                Frequency = 261.6,
+                Type = SignalGeneratorType.Sin
+                
+            }.Take(TimeSpan.FromMilliseconds(250));
+            mixer.AddMixerInput(nota_do);
         }
     }
 }
